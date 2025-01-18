41ee1e21caaca67c818b54ea0f0d7fb858ad0563
Marcus Ng <ngzhengjie888@gmail.com>
2025-01-19 02:01:56 +0800

    added expanding card and music player

---
+++ Music Player/j.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class j{
+    public static void main(String[] args) {
+        LinkedList<Integer> x = new LinkedList<Integer>();
+        x.add(26);
+        x.add(27);
+        System.out.println(x);
+    }
+}
